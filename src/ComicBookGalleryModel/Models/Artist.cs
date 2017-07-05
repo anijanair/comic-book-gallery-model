b7d96780ab4f904096a46827ebb442aaac7db6e6
Anija <anija.nair@yahoo.com>
2017-07-05 09:30:56 -0400

    Added data annotation attributes to Series, Artist and Role entities

--- src/ComicBookGalleryModel/Models/Artist.cs
+++ src/ComicBookGalleryModel/Models/Artist.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
+    
     public class Artist
     {
         public Artist()
@@ -13,6 +15,7 @@ namespace ComicBookGalleryModel.Models
             ComicBooks = new List<ComicBookArtist>();
         }
         public int Id { get; set; }
+        [Required, StringLength(100)]
         public string Name { get; set; }
 
         public ICollection<ComicBookArtist> ComicBooks { get; set; }
