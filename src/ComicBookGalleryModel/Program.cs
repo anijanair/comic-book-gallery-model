4aa8513b2771495f7013d7b6af8d4c1d56552959
Anija <anija.nair@yahoo.com>
2017-07-07 19:48:31 -0400

    Replaced 'Eager loading' with 'Lazy loading'

--- src/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/Program.cs
@@ -18,54 +18,22 @@ namespace ComicBookGalleryModel
             {
                 context.Database.Log = (message) => Debug.WriteLine(message);
 
-                //var comicBooks = context.ComicBooks
-                //    .Include(cb => cb.Series)
-                //    .OrderByDescending(cb => cb.IssueNumber)
-                //    .ThenBy(cb => cb.PublishedOn)
-                //    .ToList();
+               
+                var comicBooks = context.ComicBooks
+                   //.Include(cb => cb.Series)
+                   //.Include(cb => cb.Artists.Select(a => a.Artist))
+                   //.Include(cb => cb.Artists.Select(a => a.Role))
 
-                var comicBookQuery = context.ComicBooks
-                    .Include(cb => cb.Series)
-                    .OrderByDescending(cb => cb.IssueNumber);
-
-                var comicBooks = comicBookQuery.ToList();
-
-                var comicBooks2 = comicBookQuery
-                    .Where(cb => cb.AverageRating < 7)
-                    .ToList();
-
-
+                   .ToList();
                 foreach (var comicBook in comicBooks)
                 {
+                    var artistRoleNames = comicBook.Artists
+                        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+
                     Console.WriteLine(comicBook.DisplayText);
+                    Console.WriteLine(artistRolesDisplayText);
                 }
-                Console.WriteLine();
-                Console.WriteLine("# of comic books: {0}", comicBooks.Count);
-                Console.WriteLine();
-
-                foreach (var comicBook in comicBooks2 )
-                {
-                    Console.WriteLine(comicBook.DisplayText);
-                }
-                Console.WriteLine();
-                Console.WriteLine("# of comic books: {0}", comicBooks2.Count);
-                // To retrieve comic book
-
-                // var comicBooks = context.ComicBooks
-                //  .Include(cb => cb.Series)
-                //    .Include(cb => cb.Artists.Select(a =>a.Artist))
-                //    .Include(cb => cb.Artists.Select(a =>a.Role))
-
-                //    .ToList();
-                //foreach (var comicBook in comicBooks)
-                //{
-                //    var artistRoleNames = comicBook.Artists
-                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
-                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
-
-                //    Console.WriteLine(comicBook.DisplayText);
-                //    Console.WriteLine(artistRolesDisplayText);
-                //}
 
                 Console.ReadLine();
             }
