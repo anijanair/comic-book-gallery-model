61b2908dbbb585e5d3efcb594bf658fd0d06259f
Anija <anija.nair@yahoo.com>
2017-07-07 18:10:24 -0400

    Updated th query with filtering

--- src/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/Program.cs
@@ -17,17 +17,22 @@ namespace ComicBookGalleryModel
             using (var context = new Context())
             {
                 context.Database.Log = (message) => Debug.WriteLine(message);
-                //var comicBooks = context.ComicBooks.ToList();
 
-                var comicBooksQuery = from cb in context.ComicBooks select cb;
-                var comicBooks = comicBooksQuery.ToList();
+                var comicBooks = context.ComicBooks.Include(cb => cb.Series)
+                    .Where(cb => cb.IssueNumber == 1 || cb.Series.Title == "The Amazing Spider-Man")
+                    .ToList();
 
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+                Console.WriteLine();
                 Console.WriteLine("# of comic books: {0}", comicBooks.Count);
 
                 // To retrieve comic book
                 
-                //var comicBooks = context.ComicBooks
-                //    .Include(cb => cb.Series)
+                // var comicBooks = context.ComicBooks
+                //  .Include(cb => cb.Series)
                 //    .Include(cb => cb.Artists.Select(a =>a.Artist))
                 //    .Include(cb => cb.Artists.Select(a =>a.Role))
 
